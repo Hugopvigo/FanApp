1be17f680005bd6782cb4d8516e3aceb9a7f6188
Hugopvigo <hugopvigo@gmail.com>
2026-05-16 02:02:20 +0200

    Integrate custom colors into MaterialTheme

--- app/src/main/java/com/mediatracker/presentation/theme/Theme.kt
+++ app/src/main/java/com/mediatracker/presentation/theme/Theme.kt
@@ -16,15 +16,19 @@ import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80,
+    primary = MediaBlue,
+    secondary = MediaBlueDark,
+    tertiary = MediaOrange,
+    surface = SurfaceDark,
+    error = MediaRed,
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40,
+    primary = MediaBlue,
+    secondary = MediaBlueDark,
+    tertiary = MediaOrange,
+    surface = SurfaceLight,
+    error = MediaRed,
 )
 
 @Composable
